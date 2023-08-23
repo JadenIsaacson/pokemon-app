63cc6cdb0d6276543b52dc74664f5e84be60321e
JadenIsaacson <jfetmat19@gmail.com>
2023-08-23 11:33:46 -0500

    styled everything

--- src/app/pokemon/Card.js
+++ src/app/pokemon/Card.js
@@ -1,9 +1,16 @@
 import react from "react";
 
 const Card = ({pokemon}) => {
-    return (<div>
-        <div>{pokemon?.name}</div>
-        <div>HP: {pokemon?.stats?.[0]?.base_stat}</div>
+    console.log(pokemon)
+    let typeArray = ['lightning', 'grass', 'fire']
+    let type = typeArray?.includes(pokemon?.types?.[0]?.type?.name) ? pokemon?.types?.[0]?.type?.name : 'none'
+    return (<div className={`card-container ${type}`}>
+        <img src={pokemon?.sprites?.front_default} />
+        <div className="info-container">
+        <div className="name">{pokemon?.name}</div>
+        <div className="hp">HP: {pokemon?.stats?.[0]?.
+        base_stat}</div>
+        </div>
     </div>
     )
 }
--- src/app/pokemon/Pokemon.js
+++ src/app/pokemon/Pokemon.js
@@ -1,5 +1,5 @@
 "use client"
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Card from "./Card";
 
 const Pokemon = () => {
@@ -8,6 +8,17 @@ const Pokemon = () => {
     const [user1, setUser1] = useState('Player 1')
     const [user2, setUser2] = useState('Player 2')
     const [winner, setWinner] = useState(null)
+
+    useEffect(() => {
+        if(pokemon1?.stats?.[0]?.base_stat === pokemon2?.stats?.[0]?.base_stat) {
+            setWinner('DRAW')
+        } else if(pokemon1?.stats?.[0]?.base_stat > pokemon2?.stats?.[0]?.base_stat) {
+            setWinner(`${user1} wins!`)
+        } else {
+            setWinner(`${user2} wins!`)
+        }
+    }, [pokemon1, pokemon2, user1, user2])
+
     const handleRandom = () => {
         let randomNumber1 = Math.floor(Math.random(1, 2000) * 100)+1
 
@@ -29,13 +40,7 @@ const Pokemon = () => {
         .catch((error) => {
             console.log(error.message)
         })
-        if(pokemon1?.stats?.[0]?.base_stat === pokemon2?.stats?.[0]?.base_stat) {
-            setWinner('DRAW')
-        } else if(pokemon1?.stats?.[0]?.base_stat > pokemon2?.stats?.[0]?.base_stat) {
-            setWinner(`${user1} wins!`)
-        } else {
-            setWinner(`${user2} wins!`)
-        }
+
     }
     const handleUser1Change = (e) => {
         setUser1(e.target.value)
@@ -44,13 +49,17 @@ const Pokemon = () => {
     const handleUser2Change = (e) => {
         setUser2(e.target.value)
     }
-    return (<div>
+    let isWinner = winner && pokemon1?.stats?.[0]?.base_stat !== undefined
+    return (
+        <div>
+        <div className="container">
         <input onChange={handleUser1Change} value={user1} />
-        <Card pokemon={pokemon1}/>
-        <button onClick={handleRandom}>FIGHT!</button>
-        <Card pokemon={pokemon2}/>
+        {pokemon1 && <Card pokemon={pokemon1}/>}
+        <button className="fight" onClick={handleRandom}>FIGHT!</button>
+        {pokemon2 && <Card pokemon={pokemon2}/>}
         <input onChange={handleUser2Change} value={user2} />
-        {winner && <h1>{winner}</h1>}
+        </div>
+        {isWinner && <h1 className="winner">{winner}</h1>}
         </div>
     )
 }
